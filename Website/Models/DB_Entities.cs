abdac8369f2670baee2f9e0a5d557e7b15bdb401
legen <legen@DESKTOP-J88SUMU>
2021-12-10 15:37:08 +0200

    Добавьте файлы проекта.

---
+++ Website/Models/DB_Entities.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+
+namespace Website.Models
+{
+    public class DB_Entities : DbContext
+    {
+        public DB_Entities() : base("DB_website") { }
+        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Database.SetInitializer<demoEntities>(null);
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+    }
+
+}
---
+++ Website/ViewModel/ItemViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website.ViewModel
+{
+    public class ItemViewModel
+    {
+        public Guid ItemId { get; set; }
+        public int CategoryId { get; set; }
+        public string ItemCode { get; set; }
+        public string Decription { get; set; }
+        public decimal ItemPrice { get; set; }
+        public HttpPostedFileBase ImagePath { get; set; }
+    }
+}
